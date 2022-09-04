6e07febe2ce1a2dff0022119ee486ba805b9423a
mickymacirl <mickymacster@gmail.com>
2022-09-04 16:07:23 +0000

    add messages.py imports for styling

--- messages.py
+++ messages.py
@@ -1,1 +1,10 @@
 # Messages Python File
+''' Messages.py holds all messages returned to user '''
+
+from os import system, name
+import colorama
+from colorama import Fore
+from termcolor import colored
+from art import logo_display
+
+colorama.init(autoreset=True)
