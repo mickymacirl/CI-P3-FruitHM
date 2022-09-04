79dc0ba4eba67ddf6ea5b61db6582d6cc4e6960d
mickymacirl <mickymacster@gmail.com>
2022-09-04 16:01:20 +0000

    add words.py add fruit words split list

--- words.py
+++ words.py
@@ -1,1 +1,8 @@
-# Words Python File
+''' Holds Fruit Words '''
+
+# Fruit Words
+words = 'apple watermelon orange pear cherry strawberry' \
+    'nectarine grape mango blueberry pomegranate starfruit' \
+    'plum banana raspberry mandarin jackfruit papaya kiwi' \
+    'pineapple lime lemon apricot grapefruit' \
+    'melon coconut avocado peach'.split()
