a5176c51510e6b66ccf274dc37e2437e52c1fb59
mickymacirl <mickymacster@gmail.com>
2022-09-04 16:03:54 +0000

    add run.py import of words from words.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from words import words  # import words from words.py
 
 
 def clear():
