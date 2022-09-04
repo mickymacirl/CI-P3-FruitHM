d1d5a7997d5ee0c5fd5a94c0c4b7e7c5d0191346
mickymacirl <mickymacster@gmail.com>
2022-09-04 16:05:44 +0000

    add board.py and import to run.py for boards

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from board import board  # import board from board.py
 from words import words  # import words from words.py
 
 
