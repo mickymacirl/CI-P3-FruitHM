686b699c63fca0b3c90a8c0ef3933a02e0e9b4b4
mickymacirl <mickymacster@gmail.com>
2022-09-04 16:19:12 +0000

    add run.py colorama imports and function display_the_board

--- run.py
+++ run.py
@@ -5,8 +5,12 @@ from os import system, name
 import sys
 import random
 import time
+from colorama import init
+from colorama import Fore
 from board import board  # import board from board.py
 from words import words  # import words from words.py
+
+init(autoreset=True)  # auto reset colorama
 
 
 # define our clear function
@@ -32,8 +36,32 @@ def get_random_word(words_list):
     return words_list[list_of_words]
 
 
-def display_the_board():
-    print("Display The Board")
+def display_the_board(missing_letter, correct_guess, random_word_from_list):
+    # pass
+    """This function builds the game board from board.py and displays the
+    length of the missing letter variable"""
+    print(Fore.YELLOW + "HANGMAN: Fruit Edition(TM)")
+    print(board[len(missing_letter)])
+    print(f"{Fore.YELLOW}~-----------------------------------------~")
+    print("Letters tried:", end=" ")
+    for letter in missing_letter:
+        print(letter, end=" ")
+    print()
+    print(f"{Fore.YELLOW}~-----------------------------------------~")
+
+    empty = "~" * len(random_word_from_list)  # create empty and build from
+
+    for k_r, v_r in enumerate(random_word_from_list):
+        if v_r in correct_guess:
+            empty = empty[:k_r] + random_word_from_list[k_r] + empty[k_r + 1:]
+
+    word_length = len(random_word_from_list)
+    print(f"The word has {word_length} letters:")
+    for letter in empty:
+        # Display random word from the words
+        print(letter, end=" ")
+    print("\n")
+    print(random_word_from_list)
 
 
 def get_guess():
