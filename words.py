c3c9e96ed0ef717530127f386943caa976e942fa
mickymacirl <mickymacster@gmail.com>
2022-09-07 22:37:50 +0000

    fix words.py extra space in words to prevent word split combining two

--- words.py
+++ words.py
@@ -1,8 +1,8 @@
 ''' Holds Fruit Words '''
 
 # Fruit Words
-words = 'apple watermelon orange pear cherry strawberry' \
-    'nectarine grape mango blueberry pomegranate starfruit' \
-    'plum banana raspberry mandarin jackfruit papaya kiwi' \
-    'pineapple lime lemon apricot grapefruit' \
+words = 'apple watermelon orange pear cherry strawberry ' \
+    'nectarine grape mango blueberry pomegranate starfruit ' \
+    'plum banana raspberry mandarin jackfruit papaya kiwi ' \
+    'pineapple lime lemon apricot grapefruit ' \
     'melon coconut avocado peach'.split()
