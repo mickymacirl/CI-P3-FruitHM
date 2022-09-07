f30ba25c6c2051621d4d4105a9ff92b41b024e21
mickymacirl <mickymacster@gmail.com>
2022-09-07 21:38:31 +0000

    fix messages.py game_title for too many lines on heroku screen

--- messages.py
+++ messages.py
@@ -37,9 +37,9 @@ def game_title():
         "The origins of the game Hangman are unclear but could stretch back\n"
         "to the 1890s. Players guess letters of an unrevealed word and then \n"
         "draw an arm, leg, head or torso of a stick figure hanging from \n"
-        "gallows for every incorrect guess.\n\nIf players draw all body "
-        "parts and the word still "
-        "hasnt\nbeen spelled out, the player lose.\n"
+        "gallows for every incorrect guess. If players draw all body parts\n"
+        "and the word still hasnt been spelled out, the player will\n"
+        "lose.\n"
     )
 
 
